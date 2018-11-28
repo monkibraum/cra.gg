ede3d51912aa88455e8a66db56aa91fa4e3786a4
monkibraum <monkibraum@gmail.com>
2018-11-28 21:20:38 +0900

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { findSummoner, findTier } from './config/api';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      name: '',
+      tier : '',
+      character : '',
+    }
+  }
+
+  async componentDidMount(){
+    const id = await findSummoner('룰링머신');
+    const tier = await findTier(id);
+
+  }
+
+  onChangeName = (e) => {
+    this.setState( { 
+      name : e.target.value
+    })
+  }
+
+  search = async () => {
+
+    const id = await findSummoner(this.state.name);
+    const tier = await findTier(id);
+    this.setState({ tier : tier})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <input onChange = {this.onChangeName}/>
+        <button onClick={this.search}>검색</button>
+
+        <div> 나의 티어는 {this.state.tier} 입니다 </div>
+        {/* <div> 내가 가장 최근에 플레이한 캐릭터는 {this.state.character} 입니다 </div> */}
       </div>
     );
   }
---
+++ src/config/api.js
@@ -0,0 +1,15 @@
+import key from './apikey';
+
+
+export const findSummoner = async (name) => {
+  const first = await fetch(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/summoner/v3/summoners/by-name/${name}?api_key=${key}`) 
+  const data = await first.json();
+  return data.id
+}
+
+export const findTier = async (id) => {
+    const first = await fetch(`https://cors-anywhere.herokuapp.com/https://kr.api.riotgames.com/lol/league/v3/positions/by-summoner/${id}?api_key=${key}`) 
+    const data = await first.json();
+    return data[0].tier
+}
+
